3f8fce6d0cab572273534cc9f9dad2a2d340eb49
SantiagoLazo <selazo@sudamericano.edu.ec>
2023-11-17 20:44:21 -0500

    creacion de proyecto

---
+++ src/test/kotlin/com/example/piensa/PiensaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.piensa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PiensaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
